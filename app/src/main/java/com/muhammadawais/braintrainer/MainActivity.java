cdc6a97a1eddfb2770301299f4c0db2d5c93385c
MAwais <mawais54013@outlook.com>
2019-09-15 22:13:39 -0700

    Update MainActivity.java

--- app/src/main/java/com/muhammadawais/braintrainer/MainActivity.java
+++ app/src/main/java/com/muhammadawais/braintrainer/MainActivity.java
@@ -7,11 +7,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Button goButton;
+    ArrayList<Integer> answers = new ArrayList<Integer>();
 
     public void start(View view)
     {
@@ -26,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
 
         TextView sumTextView = findViewById(R.id.sumTextView);
 
+        Button button0 = findViewById(R.id.button0);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+
         goButton = findViewById(R.id.goButton);
 
         Random rand = new Random();
@@ -34,5 +41,28 @@ public class MainActivity extends AppCompatActivity {
         int b = rand.nextInt(21);
 
         sumTextView.setText(Integer.toString(a) + " + " + Integer.toString(b));
+
+        int locationOfCorrectAnswer = rand.nextInt(4);
+
+        for(int i = 0; i < 4; i++)
+        {
+            if(i == locationOfCorrectAnswer){
+                answers.add(a+b);
+            }
+            else
+            {
+                int wrongAnswer = rand.nextInt(41);
+
+                while(wrongAnswer == a+b) {
+                    wrongAnswer = rand.nextInt(41);
+                }
+                answers.add(rand.nextInt(41));
+            }
+        }
+
+        button0.setText(Integer.toString(answers.get(0)));
+        button1.setText(Integer.toString(answers.get(1)));
+        button2.setText(Integer.toString(answers.get(2)));
+        button3.setText(Integer.toString(answers.get(3)));
     }
 }
